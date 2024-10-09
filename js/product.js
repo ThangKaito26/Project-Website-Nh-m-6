209bcbac5773163f96b83980f33c1006ff0a299c
ThangKaito26 <athangkuroba1412@gmail.com>
2024-10-10 00:53:25 +0700

    10/10

--- js/product.js
+++ js/product.js
@@ -1,9 +1,18 @@
 const mainImage = document.getElementById('mainImage');
 const thumbnails = document.querySelectorAll('.product-thumbnails img');
 
+// Lưu trữ ảnh chính ban đầu
+const originalImageSrc = mainImage.src;
+
 thumbnails.forEach(thumbnail => {
   thumbnail.addEventListener('click', () => {
     const newImageSrc = thumbnail.getAttribute('data-image-src');
     mainImage.src = newImageSrc;
   });
+});
+
+// Thêm sự kiện click cho ảnh thumbnail đang hoạt động
+const activeThumbnail = document.querySelector('.thumbnail-active');
+activeThumbnail.addEventListener('click', () => {
+  mainImage.src = originalImageSrc; // Quay lại ảnh chính ban đầu
 });
